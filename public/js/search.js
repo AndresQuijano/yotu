0fb2429501ea75873550f4ae71c3f8e916ee89ec
Andres Quijano <andres.quijano@pragma.com.co>
2021-10-16 17:35:36 -0500

    Fix bug when search button is pressed multiple times. Add no results warning. R1

--- public/js/search.js
+++ public/js/search.js
@@ -1,29 +1,40 @@
-document.addEventListener("DOMContentLoaded",addListeners);
+document.addEventListener("DOMContentLoaded", addListeners);
 
-function addListeners(){
+function addListeners() {
     const uploadForm = document.querySelector('form');
+    uploadForm.reset();
 
     uploadForm.addEventListener('submit', goSearch);
 }
 
-async function goSearch (e) {
+async function goSearch(e) {
     e.preventDefault();
+
+    let resultTable = document.querySelector('table');
+    resultTable.innerHTML = '';
 
     const searchInput = document.querySelector('#inSearch');
 
     const criteria = searchInput.value;
     const searchURL = `/video/search/${criteria}`;
 
-    console.log('searchURL:', searchURL);
-
-    let res=await fetch(searchURL);
+    let res = await fetch(searchURL);
 
     let response = await res.json();
 
-    for(const i in response){
-        let tr=document.createElement("tr");
+    if (response.length === 0) {
+        let tr = document.createElement("tr");
+        tr.innerHTML = `<p>Nothing to show. Try a different search criteria.</p>`;
+        resultTable.appendChild(tr);
+        searchInput.value = '';
+        searchInput.focus();
+        return;
+    }
 
-        tr.innerHTML=`<a href="/video/${response[i]._id}">${response[i].name}</td>`;
-        document.querySelector('table').appendChild(tr);
+    for (const i in response) {
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `<a href="/video/${response[i]._id}">${response[i].name}</a>`;
+        resultTable.appendChild(tr);
     }
 }
